2d7b85142e53c298f939774c6f678b94a61c9de3
sho <shoxer31@gmail.com>
2021-10-29 21:46:46 -0700

    Create 2-10.py

---
+++ chap 2/2-10.py
@@ -0,0 +1,9 @@
+a = 5
+print('my fav number is ' + str(a))
+# this program prints the number by typecasting
+
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(32 / 4)
+# this program prints the value 8 by using different operators
