39c495c2f881cb0c107a80793fd09f10baea3321
sho <shoxer31@gmail.com>
2021-11-10 00:44:30 +0530

    Create 4-11.py

---
+++ chap 4/4-11.py
@@ -0,0 +1,7 @@
+a = ['pepperoni', 'farmhouse', 'cheese margherita']
+friend_pizza = a[:]
+a.append('macaroni')
+friend_pizza.append('double cheese')
+print("my friend's pizzas are " )
+for b in friend_pizza:
+    print(b)
