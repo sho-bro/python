20361647ebb5ef407de384ab89c37e6c8280673b
sho <shoxer31@gmail.com>
2021-11-09 07:56:10 +0530

    Create 3-5.py

---
+++ chap 3/3-5.py
@@ -0,0 +1,9 @@
+a = ['aritra', 'swastik', 'hulo']
+print(a[0] + ' you are invited')
+print(a[1] + ' you are invited')
+print(a[2] + ' you are invited')
+print("hulo couldn't come")
+a[2] = 'prantika'
+print(a[0] + ' you are invited')
+print(a[1] + ' you are invited')
+print(a[2] + ' you are invited')
