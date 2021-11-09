157ea59b748a8a6b778768e5459e6744625466d5
sho <shoxer31@gmail.com>
2021-11-10 00:04:55 +0530

    Create 4-1.py

---
+++ chap 4/4-1.py
@@ -0,0 +1,4 @@
+a = ['pepperoni', 'farmhouse', 'cheese margherita']
+for b in a:
+    print('I like ' + b + ' pizza') 
+print('I love all kinds of pizza be it veg or non-veg. Its a delicious italian dish. I really love pizza ')    
