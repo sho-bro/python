d80312e70c5205b382392250d78eb2d988faf929
sho <shoxer31@gmail.com>
2021-11-10 01:14:08 +0530

    Create 4-13.py

---
+++ chap 4/4-13.py
@@ -0,0 +1,6 @@
+food = ('chilli chicken', 'fried rice', 'biriyani', 'pizza', 'pasta')
+for a in food:
+    print(a)
+food = ('chilli chicken', 'fried rice', 'biriyani', 'fish fry', 'kebab roll')
+for a in food:
+    print(a)  
