7a5a6c886825e1ab5bca9a03a8ede15baafa6a8a
sho <shoxer31@gmail.com>
2021-11-10 00:39:53 +0530

    Create 4-10.py

---
+++ chap 4/4-10.py
@@ -0,0 +1,4 @@
+a = list(range(1,11))
+print('The first 3 items of the list are ' + str(a[:3]))
+print('The middle 3 items of the list are ' + str(a[3:6]))
+print('The last 3 items of the list are ' + str(a[-3:]))
