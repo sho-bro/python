b6a432c6dbd94f602757d2a6b1230709fe18688c
sho <shoxer31@gmail.com>
2021-11-09 21:08:04 -0700

    Create 5-3.py

---
+++ chap 5/5-3.py
@@ -0,0 +1,14 @@
+alien_color = 'green'
+if alien_color == 'red':
+    print('no points')
+if alien_color =='yellow':
+    print('no point')
+if alien_color == 'green':
+    print('you earned 5 points')
+alien_color = 'white'
+if alien_color == 'red':
+    print('no points')
+if alien_color =='yellow':
+    print('no point')
+if alien_color == 'green':
+    print('you earned 5 points')        
