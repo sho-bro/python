0287677d6bd82a5833c2cbe92ac54546f2e8d77d
sho <shoxer31@gmail.com>
2021-11-09 21:29:43 -0700

    Create 5-7.py

---
+++ chap 5/5-7.py
@@ -0,0 +1,11 @@
+fav_fruit = ['banana', 'apple', 'mangoe']
+if 'orange' in fav_fruit:
+    print('you really like orange')
+if 'mangoe' in fav_fruit:
+    print('you really like mangoe')
+if 'tangerine' in fav_fruit:
+    print('you really like tangerine')
+if 'grapes' in fav_fruit:
+    print('you really like grapes')            
+if 'banana' in fav_fruit:
+    print('you really like banana')
