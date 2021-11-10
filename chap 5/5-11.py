6058c63bcc430c5cd7d02942e9d1c437a8e9c3ba
sho <shoxer31@gmail.com>
2021-11-10 10:23:03 -0700

    Create 5-11.py

---
+++ chap 5/5-11.py
@@ -0,0 +1,10 @@
+a = list(range(1,10))
+for b in a:
+    if b == 1:
+        print(str(b) + "st")
+    elif b == 2:
+        print(str(b) + "nd")
+    elif b == 3:
+        print(str(b) + "rd")
+    else:
+        print(str(b) + "th")            
