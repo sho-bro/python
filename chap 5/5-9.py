f134ee8c88821c395272a8c65702b9fdfa4d7a6f
sho <shoxer31@gmail.com>
2021-11-10 10:04:06 -0700

    Create 5-9.py

---
+++ chap 5/5-9.py
@@ -0,0 +1,9 @@
+a = []
+if a:
+    for b in a:
+        if b == 'admin':
+            print('hello ' + b + ' would you like to see the status report')
+        else:
+            print("hello " + " thank you for loging again") 
+else:
+    print('need to add more members')            
