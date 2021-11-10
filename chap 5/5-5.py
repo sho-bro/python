5e0cc507497f7f23307947f568a5da611fe23906
sho <shoxer31@gmail.com>
2021-11-09 21:15:25 -0700

    Create 5-5.py

---
+++ chap 5/5-5.py
@@ -0,0 +1,21 @@
+alien_color = 'green'
+if alien_color == 'red':
+    print('you earned 10 points')
+elif alien_color =='yellow':
+    print('you earned 15 points')
+elif alien_color == 'green':
+    print('you earned 5 points')
+alien_color = 'red'
+if alien_color == 'red':
+    print('you earned 10 points')
+elif alien_color =='yellow':
+    print('you earned 15 points')
+elif alien_color == 'green':
+    print('you earned 5 points')
+alien_color = 'yellow'
+if alien_color == 'red':
+    print('you earned 10 points')
+elif alien_color =='yellow':
+    print('you earned 15 points')
+elif alien_color == 'green':
+    print('you earned 5 points')        
