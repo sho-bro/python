4983d1715ff44a5d812933f269c373e286d71b39
sho <shoxer31@gmail.com>
2021-11-10 08:37:49 +0500

    Create 5-2.py

---
+++ chap 5/5-2.py
@@ -0,0 +1,10 @@
+car = 'Audi'
+if car.lower() == 'audi':
+    print(car.lower() == 'audi')
+age = 18
+if age >= 17 or age <= 18:
+    print('adult')  
+a = ['bro', 'code']
+b = 'ok'
+if b not in a:
+    print('not ok')     
