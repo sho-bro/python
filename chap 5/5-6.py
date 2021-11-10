98b3cf0bff39ee89d6ef9448c04b689bcb5e677f
sho <shoxer31@gmail.com>
2021-11-09 21:22:32 -0700

    Create 5-6.py

---
+++ chap 5/5-6.py
@@ -0,0 +1,13 @@
+age = 25
+if age < 2:
+    print('person is a baby')
+elif age >= 2 and age < 4:
+    print('person is a toddler')
+elif age >= 4 and age < 13:
+    print('person is a kid') 
+elif age >= 13 and age < 20:
+    print("the person is a teenager")
+elif age >= 20 and age < 65:
+    print('the person is an adult')
+elif age >= 65:
+    print('the person is an elder')                          
