c295d83207169aefeca103b23ccec5ae8e21fe29
sho <shoxer31@gmail.com>
2021-11-10 10:00:13 -0700

    Create 5-8.py

---
+++ chap 5/5-8.py
@@ -0,0 +1,6 @@
+a = ['bro', 'admin', 'beji', 'nyeku', 'Mac']
+for b in a:
+    if b == 'admin':
+        print('hello ' + b + ' would you like to see the status report')
+    else:
+        print("hello " + " thank you for loging again")    
